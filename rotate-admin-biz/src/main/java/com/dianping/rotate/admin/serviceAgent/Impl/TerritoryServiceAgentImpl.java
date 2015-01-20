9595b0a5e5cbb137b12e28a12fec6ec5ff4ca15e
youjun.shen <syj2008xp@163.com>
2015-01-20 14:05:10 +0800

    add

---
+++ rotate-admin-biz/src/main/java/com/dianping/rotate/admin/serviceAgent/Impl/TerritoryServiceAgentImpl.java
@@ -0,0 +1,9 @@
+package com.dianping.rotate.admin.serviceAgent.Impl;
+
+import com.dianping.rotate.admin.serviceAgent.TerritoryServiceAgent;
+
+/**
+ * Created by shenyoujun on 15/1/20.
+ */
+public class TerritoryServiceAgentImpl implements TerritoryServiceAgent {
+}
---
+++ rotate-admin-biz/src/main/java/com/dianping/rotate/admin/serviceAgent/TerritoryServiceAgent.java
@@ -0,0 +1,9 @@
+package com.dianping.rotate.admin.serviceAgent;
+
+/**
+ * Created by shenyoujun on 15/1/20.
+ */
+public interface TerritoryServiceAgent {
+
+
+}
