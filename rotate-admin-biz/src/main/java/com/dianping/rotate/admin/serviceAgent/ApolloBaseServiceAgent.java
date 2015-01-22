4edb830a141812bd19b695b0a9bbc316a46ae2bb
youjun.shen <syj2008xp@163.com>
2015-01-22 11:07:57 +0800

    add

--- rotate-admin-biz/src/main/java/com/dianping/rotate/admin/serviceAgent/ApolloBaseServiceAgent.java
+++ rotate-admin-biz/src/main/java/com/dianping/rotate/admin/serviceAgent/ApolloBaseServiceAgent.java
@@ -1,8 +1,13 @@
 package com.dianping.rotate.admin.serviceAgent;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by shenyoujun on 15/1/21.
  */
 public interface ApolloBaseServiceAgent {
+
+    public List<Map<String,Object>> getAllBizInfo();
 
 }
--- rotate-admin-web/src/main/java/com/dianping/rotate/admin/controller/CommonDataController.java
+++ rotate-admin-web/src/main/java/com/dianping/rotate/admin/controller/CommonDataController.java
@@ -1,6 +1,7 @@
 package com.dianping.rotate.admin.controller;
 
 import com.dianping.ba.base.organizationalstructure.api.user.dto.UserDto;
+import com.dianping.rotate.admin.serviceAgent.ApolloBaseServiceAgent;
 import com.dianping.rotate.admin.serviceAgent.UserServiceAgent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by shenyoujun on 15/1/21.
@@ -21,9 +24,21 @@ public class CommonDataController {
     @Autowired
     private UserServiceAgent userServiceAgent;
 
+    @Autowired
+    private ApolloBaseServiceAgent apolloBaseServiceAgent;
+
     @RequestMapping(value = "/searchUser", method = RequestMethod.GET)
     @ResponseBody
     public List<UserDto> searchUser(@RequestParam("userName") String userName){
         return userServiceAgent.searchUser(userName);
     }
+
+    @RequestMapping(value = "/getBizInfo", method = RequestMethod.GET)
+    @ResponseBody
+    public Map getBizInfo(){
+        Map<String,Object> result = new HashMap<String, Object>();
+        result.put("bizInfo",apolloBaseServiceAgent.getAllBizInfo());
+        return result;
+    }
+
 }
