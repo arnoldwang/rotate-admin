400f967608240166a08c995cbb2c919174eb53cb
yangjie <hiyoungjay@gmail.com>
2015-01-22 13:45:05 +0800

    change url

--- rotate-admin-web/src/main/java/com/dianping/rotate/admin/controller/TerritoryController.java
+++ rotate-admin-web/src/main/java/com/dianping/rotate/admin/controller/TerritoryController.java
@@ -58,7 +58,7 @@ public class TerritoryController {
 
     }
 
-    @RequestMapping(value = "/loadTerritoryTree", method = RequestMethod.GET)
+    @RequestMapping(value = "/tree", method = RequestMethod.GET)
     @ResponseBody
     public TerritoryTreeDto loadTerritoryTree(){
         return territoryServiceAgent.loadFullTerritoryTree();
