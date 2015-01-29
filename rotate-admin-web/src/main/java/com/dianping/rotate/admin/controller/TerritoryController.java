979e8eca4eae92a4494f13d0fdf8368acf0fb011
peng.fei <peng.fei@dianping.com>
2015-01-29 16:16:31 +0800

    add

--- rotate-admin-web/src/main/java/com/dianping/rotate/admin/controller/TerritoryController.java
+++ rotate-admin-web/src/main/java/com/dianping/rotate/admin/controller/TerritoryController.java
@@ -1,18 +1,23 @@
 package com.dianping.rotate.admin.controller;
 
+import com.dianping.ba.base.organizationalstructure.api.user.UserService;
 import com.dianping.rotate.admin.exceptions.ApplicationException;
 import com.dianping.rotate.admin.serviceAgent.TerritoryServiceAgent;
 import com.dianping.rotate.admin.util.LoginUtils;
+import com.dianping.rotate.admin.utils.JsonUtils;
 import com.dianping.rotate.smt.dto.Response;
 import com.dianping.rotate.territory.dto.TerritoryDto;
 import com.dianping.rotate.territory.dto.TerritoryForWebDto;
+import com.dianping.rotate.territory.dto.TerritoryRuleItemDto;
 import com.dianping.rotate.territory.dto.TerritoryTreeDto;
+import com.google.common.collect.Maps;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by dev_wzhang on 15-1-20.
@@ -24,6 +29,9 @@ public class TerritoryController {
 
     @Autowired
     TerritoryServiceAgent territoryServiceAgent;
+
+    @Autowired
+    UserService userService;
 
     /**
      * 删除战区
@@ -46,8 +54,8 @@ public class TerritoryController {
 
     @RequestMapping(value = "", method = RequestMethod.POST)
     @ResponseBody
-    public Integer create(@RequestBody TerritoryForWebDto territoryForWebDto) throws IOException {
-
+    public Integer create(@RequestBody String data) throws IOException {
+        TerritoryForWebDto territoryForWebDto      = JsonUtils.fromStr(data, TerritoryForWebDto.class);
 
         territoryForWebDto.setOperatorId(LoginUtils.getUserLoginId());
 
@@ -96,7 +104,9 @@ public class TerritoryController {
      */
     @RequestMapping(value = "/updateTerritory", method = RequestMethod.POST)
     @ResponseBody
-    public Integer updateTerritory(@RequestBody TerritoryForWebDto territoryForWebDto) throws IOException {
+    public Integer updateTerritory(@RequestBody String data) throws IOException {
+
+        TerritoryForWebDto territoryForWebDto      = JsonUtils.fromStr(data, TerritoryForWebDto.class);
         territoryForWebDto.setOperatorId(LoginUtils.getUserLoginId());
         Response<Integer> result = territoryServiceAgent.update(territoryForWebDto);
         if(!result.isSuccess()) throw new ApplicationException("战区更新失败,"+result.getComment());
@@ -111,8 +121,33 @@ public class TerritoryController {
 
     @RequestMapping(value = "/base-info", method = RequestMethod.GET)
     @ResponseBody
-    public TerritoryForWebDto loadTerritoryBaseInfo(Integer id){
-        return  territoryServiceAgent.loadTerritoryInfoForWeb(id);
+    public Object loadTerritoryBaseInfo(Integer id){
+
+        TerritoryForWebDto t = territoryServiceAgent.loadTerritoryInfoForWeb(id);
+        if(t==null){
+            throw new ApplicationException("不存在战区%s",id);
+        }
+        Map result = Maps.newHashMap();
+        result.put("id",t.getId());
+        result.put("bizId",t.getBizId());
+        result.put("territoryName",t.getTerritoryName());
+        result.put("parentTerritoryId",t.getParentTerritoryId());
+        result.put("chiefLeaderId",t.getChiefLeaderId());
+        result.put("notOnlineMutGroupCountLimit",t.getNotOnlineMutGroupCountLimit());
+        result.put("notOnlineSingleGroupCountLimit",t.getNotOnlineSingleGroupCountLimit());
+        if(t.getParentTerritoryId()!=null){
+            TerritoryForWebDto parent = territoryServiceAgent.loadTerritoryInfoForWeb(t.getParentTerritoryId());
+            if(parent==null){
+                throw new ApplicationException("找不到父战区%s",t.getParentTerritoryId());
+            }
+            result.put("parentTerritoryName",parent.getTerritoryName());
+        }
+
+        if(t.getChiefLeaderId()!=null){
+            result.put("chiefLeaderName", userService.queryUserByLoginID(t.getChiefLeaderId()).getRealName());
+        }
+
+        return  result;
     }
 
 
